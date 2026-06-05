9f3b795b405238fa34988905548a1aa3ba7e534a
Himanshu Bhandari <himanshubhandari2191@gmail.com>
2026-06-05 16:56:05 +0530

    feat: Serve React build files from the server

--- server/index.js
+++ server/index.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path');
+
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 
 dotenv.config();
-
 connectDB();
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+// API routes
 app.get('/', (req, res) => {
     res.send('Backend is running');
 });
@@ -23,6 +26,12 @@ app.use('/api/orders', require('./routes/orderRoutes'));
 app.use('/api/payment', require('./routes/paymentRoutes'));
 app.use('/api/analytics', require('./routes/analyticsRoutes'));
 
+// 🔥 Serve React build (ADD THIS)
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 
