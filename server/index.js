1b891d38bd2ffcea4512c89d667128f9227fc7ef
Himanshu Bhandari <himanshubhandari2191@gmail.com>
2026-06-05 17:14:26 +0530

    fix: Remove unnecessary root API route for cleaner server response

--- server/index.js
+++ server/index.js
@@ -15,10 +15,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// API routes
-app.get('/', (req, res) => {
-    res.send('Backend is running');
-});
 
 app.use('/api/auth', authRoutes);
 app.use('/api/products', require('./routes/productRoutes'));
